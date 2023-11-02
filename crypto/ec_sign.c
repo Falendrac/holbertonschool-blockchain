0e70a42510deed46be2fd84bd35a2b00014d1703
Falendrac <leger_franck@protonmail.com>
2023-11-02 14:26:58 +0000

    Add documentation and fix betty style

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -1,6 +1,17 @@
 #include "hblk_crypto.h"
 
-uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t *sig)
+/**
+ * ec_sign - Sign a given set of bytes, using a given EC_KEY private key
+ *
+ * @key: The EC_KEY pair containing the private key
+ * @msg: Points to the msglen characters to be signed
+ * @msglen: Number of characters in msg
+ * @sig: The adress at whichi to store the signature
+ *
+ * Return: A pointer to the signature buffer upon succes, NULL if faillure
+*/
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
+				size_t msglen, sig_t *sig)
 {
 	unsigned int sigLen = 0;
 
